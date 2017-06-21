0348a666125fba365130bd8c1f3f97ed22469cf6
keanedawg <cfife@chicagoventure.com>
2017-06-21 14:40:10 -0500

    Displaying array of information

--- app/routes/practice.js
+++ app/routes/practice.js
@@ -2,6 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.get('store').findAll('post');
+        return [{
+            title: "Learning EmberJS",
+            author: "Erik Hatchett"
+        },{
+            title: "Controllers are Dead",
+            author: "Frank Treacy"
+        },{
+            title: "Diddly Doo",
+            author: "No I'm Adrian"
+        },{
+            title: "Ayy Lmao",
+            author: "Dank Sir"
+        }]
     }
 });
