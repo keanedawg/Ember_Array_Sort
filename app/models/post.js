c3d72ea98b87ed998bec3f54ec4d868a2c8514b1
keanedawg <cfife@chicagoventure.com>
2017-06-21 14:34:01 -0500

    added mirage.js

--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     author: DS.attr('string'),
-    title: DS.attr('string'),
-    date: DS.attr('date')
+    title: DS.attr('string')
 });
