1830cc3ad500e6dba099585a13f0dd31a5c01200
keanedawg <cfife@chicagoventure.com>
2017-06-22 15:48:03 -0500

    cleaner implementation of sorting

--- app/controllers/practice.js
+++ app/controllers/practice.js
@@ -1,41 +1,17 @@
 import Ember from 'ember';
 
-var sortByAuthor = function(a,b) {
-            if (a.author > b.author) {
-                console.log(a + " is greater than " + b);
-                return 1;
-            }
-            else {
-                console.log(b + " is greater than " + a);
-                return -1;
-            }
-        };
-
-var sortByTitle = function(a,b) {
-            if (a.title > b.title) {
-                console.log(a + " is greater than " + b);
-                return 1;
-            }
-            else {
-                console.log(b + " is greater than " + a);
-                return -1;
-            }
-        };
 
 
 
 export default Ember.Controller.extend({
+    sortKey: 'author',
+    sortedRows: Ember.computed('sortKey', function() {
+        return this.get('model').sortBy(this.get('sortKey'));
+    }),
 
     actions: {
-        sortAuthors() {
-            var myModel = this.get('model');
-            myModel.sort(sortByAuthor);
-            console.log(myModel[0].author);
-        },
-        sortTitles() {
-            var myModel = this.get('model');
-            myModel.sort(sortByTitle);
-            console.log(myModel[0].title);
+        sortBy(sortKey) {
+            this.set('sortKey', sortKey);
         }
     }
 });
--- app/routes/practice.js
+++ app/routes/practice.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        var myArray = [{
+        return [{
             title: "Learning EmberJS",
             author: "Erik Hatchett"
         },{
@@ -15,23 +15,5 @@ export default Ember.Route.extend({
             title: "Yisss",
             author: "Dank Sir"
         }];
-
-        myArray.sort(function(a,b) {
-            if (a.author > b.author) {
-                console.log(a + " is greater than " + b);
-                return 1;
-            }
-            else {
-                console.log(b + " is greater than " + a);
-                return -1;
-            }
-        });
-
-        if ("dat" < "cat") {
-            console.log("ayy");
-        }
-
-        console.log(myArray[2]);
-        return myArray;
     }
 });
