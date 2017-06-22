91139c0c0a260291871371a9826e5c45a41a2f27
keanedawg <cfife@chicagoventure.com>
2017-06-22 18:19:31 -0500

    sort by newest to oldest

--- app/controllers/practice.js
+++ app/controllers/practice.js
@@ -4,11 +4,17 @@ import Ember from 'ember';
 
 
 export default Ember.Controller.extend({
-    sortKey: 'author',
-    sortedRows: Ember.computed('sortKey', function() {
+    sortKey: 'date',
+    sortOrder: 'desc',
+  /*  sortedRows: Ember.computed('sortKey', function() {
         return this.get('model').sortBy(this.get('sortKey'));
-    }),
+    }),*/
 
+    sortedRows: Ember.computed.sort('model', 'sortKeyOrder'),
+
+//    sortKeyOrder: this.sortKey + ":" + this.sortOrder,
+
+    sortKeyOrder: ["date" + ":" + "dest"],
     actions: {
         sortBy(sortKey) {
             this.set('sortKey', sortKey);
--- app/routes/practice.js
+++ app/routes/practice.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        console.log("hello " + Date.now());
         return [{
             title: "Learning EmberJS",
             author: "Erik Hatchett",
