ba5e90d7835d5b1fa94e2e1ceb4c5d43417a059a
keanedawg <cfife@chicagoventure.com>
2017-06-22 10:57:31 -0500

    added sorting inside model function

--- app/routes/practice.js
+++ app/routes/practice.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return [{
+        var myArray = [{
             title: "Learning EmberJS",
             author: "Erik Hatchett"
         },{
@@ -14,6 +14,24 @@ export default Ember.Route.extend({
         },{
             title: "Ayy Lmao",
             author: "Dank Sir"
-        }]
+        }];
+
+        myArray.sort(function(a,b) {
+            if (a.author > b.author) {
+                console.log(a + " is greater than " + b);
+                return 1;
+            }
+            else {
+                console.log(b + " is greater than " + a);
+                return -1;
+            }
+        });
+
+        if ("dat" < "cat") {
+            console.log("ayy");
+        }
+
+        console.log(myArray[2]);
+        return myArray;
     }
 });
